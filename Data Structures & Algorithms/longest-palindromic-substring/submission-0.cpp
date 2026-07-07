bd0d3a8b156a14d9d6479c01ba6a3c79ace522c2
Nehal <159863105+nehala5@users.noreply.github.com>
2026-07-07 06:52:57 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int start = 0;
+    int maxLen = 1;
+
+    // Expands from the given center
+    void expand(string &s, int left, int right) {
+
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        // We expanded one step too far
+        int len = right - left - 1;
+
+        if (len > maxLen) {
+            maxLen = len;
+            start = left + 1;
+        }
+    }
+
+    string longestPalindrome(string s) {
+
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+
+            // Odd length palindrome (center at i)
+            expand(s, i, i);
+
+            // Even length palindrome (center between i and i+1)
+            expand(s, i, i + 1);
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
